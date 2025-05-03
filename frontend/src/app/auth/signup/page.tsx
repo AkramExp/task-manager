250cfe0e68fdfa4600ebb6faae1aa8af6d5b089b
AkramExp <akramali.jafri02@gmail.com>
2025-05-03 19:28:25 +0530

    added shadcn sonner component

--- frontend/src/app/auth/signup/page.tsx
+++ frontend/src/app/auth/signup/page.tsx
@@ -14,9 +14,16 @@ import { SignupValidation } from "@/lib/validation";
 import { zodResolver } from "@hookform/resolvers/zod";
 import Link from "next/link";
 import { useForm } from "react-hook-form";
+import { toast } from "sonner";
 import { z } from "zod";
+import axios from "axios";
+import { BACKEND_URL } from "../../../../config";
+import { log } from "node:console";
+import { useRouter } from "next/navigation";
 
 const SignUp = () => {
+  const router = useRouter();
+
   const form = useForm<z.infer<typeof SignupValidation>>({
     resolver: zodResolver(SignupValidation),
     defaultValues: {
@@ -26,7 +33,20 @@ const SignUp = () => {
     },
   });
 
-  async function onSubmit(values: z.infer<typeof SignupValidation>) {}
+  async function onSubmit(values: z.infer<typeof SignupValidation>) {
+    try {
+      const response = await axios.post(`${BACKEND_URL}/user/register`, values);
+
+      toast.success("Registered successfully");
+      localStorage.setItem("userToken", response.data.token);
+      router.push("/");
+    } catch (error: any) {
+      toast.error(
+        error.response.data.message ||
+          "Something went wrong while registering, Please try again later"
+      );
+    }
+  }
 
   return (
     <div className="flex flex-col items-center bg-white rounded-md p-6 shadow-md gap-3 max-w-md w-full mx-4">
@@ -52,7 +72,7 @@ const SignUp = () => {
                     <Input
                       placeholder="Enter your name"
                       {...field}
-                      type="email"
+                      type="text"
                       className="form-input rounded-lg border-[1px] border-blue-300 bg-blue-100/30 max-w-4xl placeholder:text-zinc-700 text-zinc-900"
                     />
                   </FormControl>
