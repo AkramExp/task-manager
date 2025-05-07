6159bdf366baa295d056084863c6e6acdc830ec0
AkramExp <akramali.jafri02@gmail.com>
2025-05-07 17:29:40 +0530

    added task list component

---
+++ frontend/src/components/TasksList.tsx
@@ -0,0 +1,28 @@
+"use client";
+
+import { useTasks } from "@/react-query/task";
+import { SearchX } from "lucide-react";
+import TaskCard from "./TaskCard";
+
+const TasksList = () => {
+  const { allTasks, isLoading } = useTasks();
+
+  return (
+    <div className="w-full px-6 py-6">
+      {allTasks?.length === 0 ? (
+        <div className="w-full flex items-center justify-center gap-4 text-2xl sm:text-3xl font-bold text-gray-400">
+          <SearchX className="w-8 h-8 sm:w-10 sm:h-10" />
+          No Tasks Found
+        </div>
+      ) : (
+        <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-6">
+          {allTasks?.map((task: any) => (
+            <TaskCard key={task._id} task={task} />
+          ))}
+        </div>
+      )}
+    </div>
+  );
+};
+
+export default TasksList;
