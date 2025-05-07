a83ab1a0fa6bc4abdd2bc8f8684ff30f169b61c5
AkramExp <akramali.jafri02@gmail.com>
2025-05-07 17:28:29 +0530

    added task and status vaidations

--- frontend/src/lib/validation/index.ts
+++ frontend/src/lib/validation/index.ts
@@ -16,3 +16,16 @@ export const SigninValidation = z.object({
     .min(8, { message: "Password must be at least 8 characters" })
     .max(20),
 });
+
+export const TaskValidation = z.object({
+  title: z.string(),
+  description: z.string().optional(),
+  dueDate: z.date().optional(),
+  priority: z.string(),
+  status: z.string(),
+  assignedTo: z.string().optional(),
+});
+
+export const StatusValidation = z.object({
+  status: z.string(),
+});
