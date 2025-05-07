28bc231240ec7b2687bc86fd51f4ad92c383c62d
AkramExp <akramali.jafri02@gmail.com>
2025-05-07 17:31:16 +0530

    added dashboard page and layout

---
+++ frontend/src/app/(root)/dashboard/page.tsx
@@ -0,0 +1,25 @@
+import SearchInput from "@/components/SearchInput";
+import TasksList from "@/components/TasksList";
+import CreateNewTask from "@/components/CreateNewTask";
+import Filters from "@/components/Filters";
+
+const Page = () => {
+  return (
+    <div className="flex flex-col items-center pt-[6rem] min-h-screen bg-gray-900 text-white py-6">
+      <div className="flex flex-col gap-6 max-w-[700px] w-full px-4">
+        <SearchInput />
+
+        <div className="flex flex-col items-center gap-6 justify-between px-8">
+          <Filters />
+          <CreateNewTask />
+        </div>
+      </div>
+
+      <div className="w-full mt-6 px-4">
+        <TasksList />
+      </div>
+    </div>
+  );
+};
+
+export default Page;
