ad8bdf2ad8bf985b8e82ae564028e5ac30bd2458
AkramExp <akramali.jafri02@gmail.com>
2025-05-08 17:54:29 +0530

    added the logo and favicon and renamed the application

--- frontend/src/components/Navbar.tsx
+++ frontend/src/components/Navbar.tsx
@@ -2,13 +2,24 @@ import Link from "next/link";
 import LogoutButton from "./LogoutButton";
 import Notifications from "./Notifications";
 import { User } from "lucide-react";
+import Image from "next/image";
 
 const Navbar = () => {
   return (
     <div className="fixed top-0 w-full bg-gray-800 text-white shadow-md">
       <div className="flex items-center justify-between py-4 px-6">
-        <Link href="/dashboard" className="text-xl sm:text-2xl font-semibold">
-          Task Manager
+        <Link
+          href="/dashboard"
+          className="text-xl sm:text-2xl font-semibold flex items-center gap-3"
+        >
+          <Image
+            src={"/logo.png"}
+            width={30}
+            height={30}
+            alt="logo"
+            className="rounded-sm"
+          />
+          <p className="hidden sm:block">TaskFlow</p>
         </Link>
         <div className="flex items-center gap-2">
           <Link href="/user">
