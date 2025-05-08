dc31708e8c10df666d29a098638402c8a67d7caa
AkramExp <akramali.jafri02@gmail.com>
2025-05-08 13:15:40 +0530

    wrapped dashboard in suspense tag

--- frontend/src/app/(root)/dashboard/page.tsx
+++ frontend/src/app/(root)/dashboard/page.tsx
@@ -2,23 +2,26 @@ import CreateNewTask from "@/components/CreateNewTask";
 import Filters from "@/components/Filters";
 import SearchInput from "@/components/SearchInput";
 import TasksList from "@/components/TasksList";
+import { Suspense } from "react";
 
 const Page = () => {
   return (
-    <div className="flex flex-col items-center pt-[6rem] min-h-screen bg-gray-900 text-white py-6">
-      <div className="flex flex-col gap-6 max-w-[700px] w-full px-4">
-        <SearchInput />
+    <Suspense>
+      <div className="flex flex-col items-center pt-[6rem] min-h-screen bg-gray-900 text-white py-6">
+        <div className="flex flex-col gap-6 max-w-[700px] w-full px-4">
+          <SearchInput />
 
-        <div className="flex flex-col items-center gap-6 justify-between px-8">
-          <Filters />
-          <CreateNewTask />
+          <div className="flex flex-col items-center gap-6 justify-between px-8">
+            <Filters />
+            <CreateNewTask />
+          </div>
+        </div>
+
+        <div className="w-full mt-6 md:px-4">
+          <TasksList />
         </div>
       </div>
-
-      <div className="w-full mt-6 md:px-4">
-        <TasksList />
-      </div>
-    </div>
+    </Suspense>
   );
 };
 
