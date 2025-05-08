9a1dd8cd2d312296f2b44aed0241153b084e0c30
AkramExp <akramali.jafri02@gmail.com>
2025-05-08 17:22:55 +0530

    added react query hooks for update task status, create task and update task api calls

--- frontend/src/react-query/task.ts
+++ frontend/src/react-query/task.ts
@@ -1,15 +1,18 @@
 "use client";
 
-import { useQuery } from "@tanstack/react-query";
+import { useMutation, useQuery, useQueryClient } from "@tanstack/react-query";
 import { BACKEND_URL } from "../../config";
 import axios from "axios";
 import Cookies from "js-cookie";
 import { toast } from "sonner";
 import { useSearchParams } from "next/navigation";
+import { StatusValidation, TaskValidation } from "@/lib/validation";
+import { z } from "zod";
+
+const token = Cookies.get("userToken");
 
 export function useTasks() {
   const searchParams = useSearchParams();
-
   const token = Cookies.get("userToken");
 
   const { data: allTasks, isLoading } = useQuery({
@@ -44,4 +47,96 @@ export function useTasks() {
   });
 
   return { allTasks, isLoading };
+}
+
+export function useUpdateTaskStatus(
+  taskId: string,
+  setToggleForm: (value: boolean) => void
+) {
+  const queryClient = useQueryClient();
+
+  const { mutate: updateStatus, isPending } = useMutation({
+    mutationFn: async (payload: z.infer<typeof StatusValidation>) => {
+      const { data } = await axios.put(
+        `${BACKEND_URL}/task/update-status/${taskId}`,
+        payload,
+        {
+          withCredentials: true,
+          headers: { Authorization: `Bearer ${token}` },
+        }
+      );
+
+      return data;
+    },
+    onSuccess: (data) => {
+      toast.success(data.message);
+      queryClient.invalidateQueries({ queryKey: ["all-tasks"] });
+      setToggleForm(false);
+    },
+    onError: (error: any) => {
+      toast.error(
+        error?.response?.data?.message ||
+          "Something went wrong updating status, Please try again later"
+      );
+    },
+  });
+
+  return { updateStatus, isPending };
+}
+
+export function useCreateOrUpdateTask(
+  taskId: string,
+  setToggleForm: (value: boolean) => void,
+  type: string
+) {
+  const queryClient = useQueryClient();
+
+  const { mutate: createOrUpdateTask, isPending } = useMutation({
+    mutationFn: async ({
+      payload,
+      type,
+    }: {
+      payload: z.infer<typeof TaskValidation>;
+      type: string;
+    }) => {
+      if (type === "edit") {
+        const { data } = await axios.put(
+          `${BACKEND_URL}/task/update/${taskId}`,
+          payload,
+          {
+            withCredentials: true,
+            headers: { Authorization: `Bearer ${token}` },
+          }
+        );
+
+        return data;
+      } else {
+        const { data } = await axios.post(
+          `${BACKEND_URL}/task/create`,
+          payload,
+          {
+            withCredentials: true,
+            headers: { Authorization: `Bearer ${token}` },
+          }
+        );
+
+        return data;
+      }
+    },
+    onSuccess: (data) => {
+      toast.success(data.message);
+      queryClient.invalidateQueries({ queryKey: ["all-tasks"] });
+      setToggleForm(false);
+    },
+    onError: (error: any) => {
+      toast.error(
+        error?.response?.data?.message ||
+          `Something went wrong ${
+            type === "edit" ? "editing" : "creating"
+          } task, Please try again later`
+      );
+    },
+  });
+
+  return { createOrUpdateTask, isPending };
 }
