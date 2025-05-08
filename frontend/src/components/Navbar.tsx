36ac2ddc4090824d1912bd90acd94ec62656b0c4
AkramExp <akramali.jafri02@gmail.com>
2025-05-08 12:51:38 +0530

    added user icon for the user profile on the navbar

--- frontend/src/components/Navbar.tsx
+++ frontend/src/components/Navbar.tsx
@@ -1,16 +1,19 @@
-import { LogOut } from "lucide-react";
 import Link from "next/link";
 import LogoutButton from "./LogoutButton";
 import Notifications from "./Notifications";
+import { User } from "lucide-react";
 
 const Navbar = () => {
   return (
     <div className="fixed top-0 w-full bg-gray-800 text-white shadow-md">
       <div className="flex items-center justify-between py-4 px-6">
-        <Link href="/" className="text-xl sm:text-2xl font-semibold">
+        <Link href="/dashboard" className="text-xl sm:text-2xl font-semibold">
           Task Manager
         </Link>
         <div className="flex items-center gap-2">
+          <Link href="/user">
+            <User className="mr-2" />
+          </Link>
           <Notifications />
           <LogoutButton />
         </div>
